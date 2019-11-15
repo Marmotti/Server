feae3ece670c3ddedee3af07cbc96beacb1ab6bd
Marmotti <playmarmotti@gmail.com>
2019-11-15 11:03:49 +0100

    Changed a bit for video receiving

--- src/ThreadHandler.java
+++ src/ThreadHandler.java
@@ -1,13 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ThreadHandler extends Thread{
 
     private Socket newSocket;
     private int n;
+    ObjectOutputStream out;
+    ObjectInputStream in;
 
     ThreadHandler(Socket newSocket, int n) {
         this.newSocket = newSocket;
@@ -17,22 +16,16 @@ public class ThreadHandler extends Thread{
     public void run() {
         try {
             PrintWriter output = new PrintWriter(newSocket.getOutputStream(), true);
-            BufferedReader input = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
             output.println("Hello :: enter QUIT to exit \n");
             boolean moreData = true;
             String newLine;
 
             while (moreData){
-                newLine = input.readLine();
-                System.out.println("Message '" + newLine +"' echoed back to client.");
-                if (newLine == null){
+                in = new ObjectInputStream(newSocket.getInputStream());
+                System.out.println("Message received.");
+                if (in == null){
                     System.out.println("line == null lol");
                     moreData = false;
-                }
-                else {
-                    output.println("From server: " + newLine + ". \n");
-                    if (newLine.trim().equals("QUIT"))
-                        moreData = false;
                 }
             }
 
